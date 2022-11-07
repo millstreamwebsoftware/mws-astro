042c3d93e5cd9b642c12f445194a12ccb23bbb1d
oliverlynch <59594611+oliverlynch@users.noreply.github.com>
2022-11-07 13:55:35 +0800

    Slider fixes

--- types.ts
+++ types.ts
@@ -15,43 +15,44 @@ type Gallery = {
     images: GalleryItem[];
 }
 
-type FlickityOptions = {
-    draggable?: object;
-    freeScroll?: boolean;
-    wrapAround?: boolean;
-    groupCells?: object;
-    autoPlay?: object;
-    fullscreen?: boolean;
-    fade?: boolean;
-    adaptiveHeight?: boolean;
-    watchCSS?: boolean;
-    asNavFor?: object;
-    hash?: boolean;
-    dragThreshold?: number;
-    selectedAttraction?: number;
-    friction?: number;
-    freeScrollFriction?: number;
-    imagesLoaded?: boolean;
-    lazyLoad?: object;
-    bgLazyLoad?: object;
-    cellSelector?: string;
-    initialIndex?: object;
-    accessibility?: boolean;
-    setGallerySize?: boolean;
-    resize?: boolean;
-    cellAlign?: string;
-    contain?: boolean;
-    percentPosition: object;
-    rightToLeft?: boolean;
-    prevNextButtons?: boolean;
-    pageDots?: boolean;
-    arrowShape?: string;
-}
+// type FlickityOptions = {
+//     draggable?: object;
+//     freeScroll?: boolean;
+//     wrapAround?: boolean;
+//     groupCells?: object;
+//     autoPlay?: object;
+//     fullscreen?: boolean;
+//     fade?: boolean;
+//     adaptiveHeight?: boolean;
+//     watchCSS?: boolean;
+//     asNavFor?: object;
+//     hash?: boolean;
+//     dragThreshold?: number;
+//     selectedAttraction?: number;
+//     friction?: number;
+//     freeScrollFriction?: number;
+//     imagesLoaded?: boolean;
+//     lazyLoad?: object;
+//     bgLazyLoad?: object;
+//     cellSelector?: string;
+//     initialIndex?: object;
+//     accessibility?: boolean;
+//     setGallerySize?: boolean;
+//     resize?: boolean;
+//     cellAlign?: string;
+//     contain?: boolean;
+//     percentPosition: object;
+//     rightToLeft?: boolean;
+//     prevNextButtons?: boolean;
+//     pageDots?: boolean;
+//     arrowShape?: string;
+// }
+
 
 type Slider = {
     images: GalleryItem[];
     imageFit?: string;
-    options: FlickityOptions;
+    options: SwiperOptions;
 }
 
 type Panel = {
@@ -65,7 +66,6 @@ type Panel = {
 export type {
     GalleryItem,
     Gallery,
-    FlickityOptions,
     Slider,
     Panel
 }
