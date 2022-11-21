12129d60b1387696f1ab36384167cfbe465814f8
Adrian Lynch <adrian@adrianlynch.dev>
2022-11-21 17:12:55 +0800

    Added site config

---
+++ config.ts
@@ -0,0 +1,25 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE = {
+  title: 'The Oar House | Cafe on the Swan River Bayswater',
+  description: 'Cafe on the banks of the Swan River in Bayswater',
+  canonical: 'https://www.theoarhouse.au',
+  social: [
+    {
+      name: 'facebook',
+      title: 'Facebook',
+      link: 'https://www.facebook.com/theoarhouse.au',
+    },
+    {
+      name: 'instagram',
+      title: 'Instagram',
+      link: 'https://www.instagram.com/theoarhouse.au',
+    },
+    {
+      name: 'twitter',
+      title: 'Twitter',
+      link: 'https://twitter.com/theoarhouse.au',
+    }
+  ],
+};
