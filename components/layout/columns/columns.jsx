1b3664c7ee4f90ab083bd3202cedde9549558554
oliverlynch <59594611+oliverlynch@users.noreply.github.com>
2023-06-15 15:52:39 +0800

    Test columns in live editor

--- components/layout/columns/columns.jsx
+++ components/layout/columns/columns.jsx
@@ -1,11 +1,28 @@
+const components = {};
+const componentImports = import.meta.glob("../../**/*.jsx", {
+  eager: true,
+});
+Object.entries(componentImports).forEach(([path, obj]) => {
+  const parts = path.replace("../../", "").split(".")[0].split("/");
+  if (parts.length > 1 && parts[parts.length - 1] === parts[parts.length - 2]) {
+    parts.pop();
+  }
+  const bookshopName = parts.join("/");
+  components[bookshopName] = obj.default;
+});
+
 export default function Columns(block) {
   return (
     <section className="columns" style={{"--column-count": block.content_blocks.length}}>
-      { block.content_blocks.map((column, i) => (
-        <div className={`column${i}`}>
-          <p>Column Test</p>
-        </div>
-      ))}
+      { block.content_blocks.map((column, i) => {
+        const Component = components[column._bookshop_name];
+        console.log(column);
+        return (
+          <div className={`column${i}`}>
+            <Component {...column} key={i} />
+          </div>
+        );
+      })}
     </section>
   );
 }
