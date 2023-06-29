bab4160159376c22d89a8ff129e75e547696a81f
oliverlynch <59594611+oliverlynch@users.noreply.github.com>
2023-06-29 16:26:31 +0800

    Check environment flag type

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="unplugin-icons/types/astro" />
-
-interface ImportMetaEnv {
-  readonly ENV_BOOKSHOP_LIVE: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
