7bbb089fc00d91a7f2c1b36a174013a78ca47bda
oliverlynch <59594611+oliverlynch@users.noreply.github.com>
2023-06-29 15:21:36 +0800

    Use sample component verbatim

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="unplugin-icons/types/astro" />
+
+interface ImportMetaEnv {
+  readonly ENV_BOOKSHOP_LIVE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
