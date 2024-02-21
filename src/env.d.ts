42dcae08aefb9837d9a8b31b6244575014ed55af
oliverlynch <59594611+oliverlynch@users.noreply.github.com>
2024-02-21 15:12:28 +0800

    Add preferences.json typing in env.d.t

--- src/env.d.ts
+++ src/env.d.ts
@@ -20,4 +20,43 @@ declare global {
   interface MapElement extends HTMLElement {
     map?: any;
   }
+
+  interface Preferences {
+    general: {
+      site_name: string;
+      canonical_url: string;
+      browser_window_title_suffix: string;
+      default_site_description: string | null;
+      default_sharing_image: string | null;
+      favicon: string;
+      social: {
+        name?: string;
+        title?: string;
+        link?: string;
+        icon?: string | undefined;
+      }[];
+    };
+    header: {
+      logo_image: string;
+      pin_header: boolean;
+      navigation_menu_depth: number;
+      show_logo: boolean;
+      show_navigation: boolean;
+      show_socials: boolean;
+    };
+    footer: {
+      foreground_color?: string;
+      background_color?: string;
+      copyright: string;
+      show_copyright: boolean;
+      show_socials: boolean;
+      show_top_button: boolean;
+    };
+    API: {
+      google_maps?: { api_key: string };
+      google_calendar?: { api_key: string };
+      imgix?: { domain: string; token: string };
+    };
+    _input: Record<string, any>;
+  }
 }
