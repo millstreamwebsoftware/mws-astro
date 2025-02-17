af023fc1d471fdcf76d2310632a14e3d2d7e334f
oliverlynch <59594611+oliverlynch@users.noreply.github.com>
2025-02-17 16:37:11 +0800

    fix(pdf): render non-embedded fonts in pdf thumbnails

--- src/pdf.ts
+++ src/pdf.ts
@@ -3,10 +3,9 @@
 // import sharp from "sharp";
 // import fs from "fs";
 import { resolvePath } from "./filesystem";
-var fs: any, pdfjs: any;
+var fs: any, pdfjs: any, path: any;
 
 // https://github.com/mozilla/pdf.js/blob/master/examples/node/pdf2png/pdf2png.mjs
-
 // import { getDocument } from "pdfjs-dist/legacy/build/pdf.mjs";
 
 async function ensureFS(): Promise<boolean> {
@@ -19,25 +18,35 @@ async function ensureFS(): Promise<boolean> {
 
     if (!pdfjs)
       pdfjs = await import("pdfjs-dist/legacy/build/pdf.mjs").catch(() =>
-        console.error("node:fs import failed"),
+        console.error("pdfjs-dist/legacy import failed"),
       );
 
-    return Boolean(fs) && Boolean(pdfjs);
+    if (!path)
+      path = await import("path").catch(() =>
+        console.error("path import failed"),
+      );
+
+    return Boolean(fs) && Boolean(pdfjs) && Boolean(path);
   }
 
   return false;
 }
 
-// Some PDFs need external cmaps.
-const CMAP_URL = "../../../node_modules/pdfjs-dist/cmaps/";
-const CMAP_PACKED = true;
-
-// Where the standard fonts are located.
-const STANDARD_FONT_DATA_URL =
-  "../../../node_modules/pdfjs-dist/standard_fonts/";
-
 export async function getPDFThumbnail(file: string, pageNum: number = 0) {
   if (!(await ensureFS())) return;
+
+  // Some PDFs need external cmaps.
+  const CMAP_URL = path.join(
+    import.meta.dirname,
+    "../../../../node_modules/pdfjs-dist/cmaps/",
+  );
+  const CMAP_PACKED = true;
+
+  // Where the standard fonts are located.
+  const STANDARD_FONT_DATA_URL = path.join(
+    import.meta.dirname,
+    "../../../../node_modules/pdfjs-dist/standard_fonts/",
+  );
 
   const filePath = await resolvePath(file);
   if (!filePath) return;
@@ -53,6 +62,7 @@ export async function getPDFThumbnail(file: string, pageNum: number = 0) {
     cMapUrl: CMAP_URL,
     cMapPacked: CMAP_PACKED,
     standardFontDataUrl: STANDARD_FONT_DATA_URL,
+    disableFontFace: false,
     verbosity: 0,
   });
 
