5fb753ae4cc796e9c8b53a874ba2180b4bef01d3
oliverlynch <59594611+oliverlynch@users.noreply.github.com>
2025-03-11 17:11:15 +0800

    fix(pdf): fix bookshop guard statement

--- src/pdf.ts
+++ src/pdf.ts
@@ -2,45 +2,45 @@ import { resolvePath } from "./filesystem";
 
 export async function getPDFThumbnail(file: string, pageNum = 0) {
   // @ts-expect-error
-  if (ENV_BOOKSHOP_LIVE) return;
+  if (!ENV_BOOKSHOP_LIVE) {
+    const [fs, pdf, sharp] = await Promise.all([
+      import("node:fs"),
+      import("mupdf"),
+      import("sharp"),
+    ]);
 
-  const [fs, pdf, sharp] = await Promise.all([
-    import("node:fs"),
-    import("mupdf"),
-    import("sharp"),
-  ]);
+    const filePath = await resolvePath(file);
+    if (!filePath) return;
 
-  const filePath = await resolvePath(file);
-  if (!filePath) return;
+    let thumbPath = `${filePath}.webp`;
 
-  let thumbPath = `${filePath}.webp`;
+    if (fs.existsSync(thumbPath)) return thumbPath;
+    const time = performance.now();
 
-  if (fs.existsSync(thumbPath)) return thumbPath;
-  const time = performance.now();
+    const doc = pdf.PDFDocument.openDocument(
+      fs.readFileSync(filePath),
+      "application/pdf",
+    );
 
-  const doc = pdf.PDFDocument.openDocument(
-    fs.readFileSync(filePath),
-    "application/pdf",
-  );
+    const page = doc.loadPage(pageNum);
 
-  const page = doc.loadPage(pageNum);
+    let pixmap = page.toPixmap(
+      pdf.Matrix.scale(0.5, 0.5),
+      pdf.ColorSpace.DeviceRGB,
+      false,
+      true,
+    );
 
-  let pixmap = page.toPixmap(
-    pdf.Matrix.scale(0.5, 0.5), // pdf.Matrix.identity,
-    pdf.ColorSpace.DeviceRGB,
-    false,
-    true,
-  );
+    await sharp
+      .default(pixmap.asPNG())
+      .resize(720, 720, { fit: "inside" })
+      .webp({ quality: 60 })
+      .toFile(thumbPath);
 
-  await sharp
-    .default(pixmap.asPNG())
-    .resize(720, 720, { fit: "inside" })
-    .webp({ quality: 60 })
-    .toFile(thumbPath);
+    console.log(
+      `[PDF Thumbnailer] Generated ${thumbPath} in ${performance.now() - time}ms`,
+    );
 
-  console.log(
-    `[PDF Thumbnailer] Generated ${thumbPath} in ${performance.now() - time}ms`,
-  );
-
-  return thumbPath;
+    return thumbPath;
+  }
 }
