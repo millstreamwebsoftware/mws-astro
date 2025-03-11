f4bd2b3f689d80a5a5df166eee4475d003554133
oliverlynch <59594611+oliverlynch@users.noreply.github.com>
2025-03-11 17:01:35 +0800

    feat(pdf)!: replace pdfjs with mupdf for improved thumbnail rendering

--- src/pdf.ts
+++ src/pdf.ts
@@ -1,108 +1,46 @@
-// import fs from "fs/promises";
-// import path from "path";
-// import sharp from "sharp";
-// import fs from "fs";
 import { resolvePath } from "./filesystem";
-var fs: any, pdfjs: any, path: any;
 
-// https://github.com/mozilla/pdf.js/blob/master/examples/node/pdf2png/pdf2png.mjs
-// import { getDocument } from "pdfjs-dist/legacy/build/pdf.mjs";
-
-async function ensureFS(): Promise<boolean> {
+export async function getPDFThumbnail(file: string, pageNum = 0) {
   // @ts-expect-error
-  if (!ENV_BOOKSHOP_LIVE) {
-    if (!fs)
-      fs = await import("node:fs").catch(() =>
-        console.error("node:fs import failed"),
-      );
+  if (ENV_BOOKSHOP_LIVE) return;
 
-    if (!pdfjs)
-      pdfjs = await import("pdfjs-dist/legacy/build/pdf.mjs").catch(() =>
-        console.error("pdfjs-dist/legacy import failed"),
-      );
-
-    if (!path)
-      path = await import("path").catch(() =>
-        console.error("path import failed"),
-      );
-
-    return Boolean(fs) && Boolean(pdfjs) && Boolean(path);
-  }
-
-  return false;
-}
-
-export async function getPDFThumbnail(file: string, pageNum: number = 0) {
-  if (!(await ensureFS())) return;
-
-  // Some PDFs need external cmaps.
-  const CMAP_URL = path.join(
-    import.meta.dirname,
-    "../../../../node_modules/pdfjs-dist/cmaps/",
-  );
-  const CMAP_PACKED = true;
-
-  // Where the standard fonts are located.
-  const STANDARD_FONT_DATA_URL = path.join(
-    import.meta.dirname,
-    "../../../../node_modules/pdfjs-dist/standard_fonts/",
-  );
+  const [fs, pdf, sharp] = await Promise.all([
+    import("node:fs"),
+    import("mupdf"),
+    import("sharp"),
+  ]);
 
   const filePath = await resolvePath(file);
   if (!filePath) return;
 
-  let thumbPath = `${filePath}.png`;
+  let thumbPath = `${filePath}.webp`;
 
   if (fs.existsSync(thumbPath)) return thumbPath;
+  const time = performance.now();
 
-  const data = new Uint8Array(fs.readFileSync(filePath));
+  const doc = pdf.PDFDocument.openDocument(
+    fs.readFileSync(filePath),
+    "application/pdf",
+  );
 
-  const loadingTask = pdfjs.getDocument({
-    data,
-    cMapUrl: CMAP_URL,
-    cMapPacked: CMAP_PACKED,
-    standardFontDataUrl: STANDARD_FONT_DATA_URL,
-    disableFontFace: false,
-    verbosity: 0,
-  });
+  const page = doc.loadPage(pageNum);
 
-  try {
-    const pdfDocument = await loadingTask.promise;
-    // console.log("# PDF document loaded.");
-    // Get the first page.
-    const page = await pdfDocument.getPage(pageNum + 1);
-    // Render the page on a Node canvas with 100% scale.
-    const canvasFactory: any = pdfDocument.canvasFactory;
-    const viewport = page.getViewport({ scale: 1.0 });
-    const canvasAndContext = canvasFactory.create(
-      viewport.width,
-      viewport.height,
-    );
-    const renderContext = {
-      canvasContext: canvasAndContext.context,
-      viewport,
-    };
+  let pixmap = page.toPixmap(
+    pdf.Matrix.scale(0.5, 0.5), // pdf.Matrix.identity,
+    pdf.ColorSpace.DeviceRGB,
+    false,
+    true,
+  );
 
-    const renderTask = page.render(renderContext);
-    await renderTask.promise;
-    // Convert the canvas to an image buffer.
-    const image = canvasAndContext.canvas.toBuffer("image/png");
-    fs.writeFile(thumbPath, image, function (error: any) {
-      if (error) {
-        // console.error("Error: " + error);
-      } else {
-        // console.log(
-        //   "Finished converting first page of PDF file to a PNG image.",
-        // );
-      }
-    });
-    // Release page resources.
-    page.cleanup();
-  } catch (reason) {
-    // console.log(reason);
-  }
+  await sharp
+    .default(pixmap.asPNG())
+    .resize(720, 720, { fit: "inside" })
+    .webp({ quality: 60 })
+    .toFile(thumbPath);
 
-  // console.log(thumbPath);
+  console.log(
+    `[PDF Thumbnailer] Generated ${thumbPath} in ${performance.now() - time}ms`,
+  );
 
   return thumbPath;
 }
