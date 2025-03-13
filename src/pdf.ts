6109bbce8a6b1b2280372261b6bb4089a26f5953
oliverlynch <59594611+oliverlynch@users.noreply.github.com>
2025-03-13 14:40:40 +0800

    feat: allow generating multiple sizes of PDF thumbnail

--- src/pdf.ts
+++ src/pdf.ts
@@ -1,18 +1,22 @@
 import { resolvePath } from "./filesystem";
 
-export async function getPDFThumbnail(file: string, pageNum = 0) {
+export async function getPDFThumbnail(file: string, pageNum = 0, res = 512) {
   // @ts-expect-error
   if (!ENV_BOOKSHOP_LIVE) {
-    const [fs, pdf, sharp] = await Promise.all([
+    const [fs, pdf, sharp, crypto] = await Promise.all([
       import("node:fs"),
       import("mupdf"),
       import("sharp"),
+      import("crypto"),
     ]);
 
     const filePath = await resolvePath(file);
     if (!filePath) return;
 
-    let thumbPath = `${filePath}.webp`;
+    const hash = crypto.createHash("md5");
+
+    hash.update(`${file}${pageNum}${res}`, "utf-8");
+    let thumbPath = `${filePath}.${hash.digest("hex").slice(0, 6)}.webp`;
 
     if (fs.existsSync(thumbPath)) return thumbPath;
     const time = performance.now();
@@ -24,21 +28,21 @@ export async function getPDFThumbnail(file: string, pageNum = 0) {
 
     const page = doc.loadPage(pageNum);
 
+    const [, , bound_w, bound_h] = page.getBounds();
+    const [scale_w, scale_h] = [res / bound_w, res / bound_h];
+    const scale = Math.min(scale_w, scale_h);
+
     let pixmap = page.toPixmap(
-      pdf.Matrix.scale(0.5, 0.5),
+      pdf.Matrix.scale(scale, scale),
       pdf.ColorSpace.DeviceRGB,
       false,
       true,
     );
 
-    await sharp
-      .default(pixmap.asPNG())
-      .resize(720, 720, { fit: "inside" })
-      .webp({ quality: 60 })
-      .toFile(thumbPath);
+    await sharp.default(pixmap.asPNG()).webp({ quality: 60 }).toFile(thumbPath);
 
     console.log(
-      `[PDF Thumbnailer] Generated ${thumbPath} in ${performance.now() - time}ms`,
+      `[PDF Thumbnailer] Generated ${thumbPath} in ${(performance.now() - time).toFixed(0)}ms`,
     );
 
     return thumbPath;
